aec9138cd1808e8de1cb1524c9ef3d05f04a64f6
A7med-sharaf <a9986939@gmail.com>
2024-08-11 14:23:25 +0300

    edit

--- style.js
+++ style.js
@@ -7,12 +7,57 @@ let x=prompt("أدخل اسم المحافظة :");
 
 switch(x){
 
-case "القاهرة":console.log("30% خصم");break
-case "الاسكندرية":console.log("20% خصم ");break
-case "اسوان":console.log("50% خصم");break
-default :console.log("معليش المحافظة دي مش  معانا");
-
-
+case "القاهرة":console.log("5% خصم");break
+case "الاسكندرية":console.log("10% خصم ");break
+case "اسوان":console.log("30% خصم");break
+default :console.log("40% خصم");
 
 
 }
+
+
+
+
+// let email=prompt("inter your email")
+// let pass=+prompt("inter your password")
+
+// if(email == "ahmed@gmail.com" && pass == 12345 ){
+//     alert("success")
+// }else if(email == "amjed@gmail.com" && pass == 12345 ){
+//         alert("success")
+//     }
+// else{
+//     alert("erro email or password")
+// }
+
+
+// let money=+prompt("ادخل قيمة الميراث:")
+// let wifevalue=confirm("هل لديك زوجة :")
+
+//  wifeindex=0;
+
+
+// if(wifevalue == true){
+//     wifeindex=money * 0.125;
+//      money=money - wifeindex;
+   
+// }
+
+// let male=+prompt("لديك كم ولد")
+// let female=+prompt("لديك كم بنت")
+
+// let sahmev=male*2+female
+
+// let finalv=money/sahmev
+
+// let meg=(`
+    
+//     الميراث ${money}
+//     ميراث الازوجة ${wifeindex}
+//     ميراث االينت${finalv}
+//     ميراث الاواد ${finalv*2}
+    
+//     `)
+
+//     console.log(meg);
+    
