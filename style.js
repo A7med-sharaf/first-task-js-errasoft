b5cdf5eebaeb1a403d7a54e1578499d93bf3ea36
A7med-sharaf <a9986939@gmail.com>
2024-08-11 10:37:36 +0300

    js

---
+++ style.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+let x=prompt("أدخل اسم المحافظة :");
+
+switch(x){
+
+case "القاهرة":console.log("30% خصم");break
+case "الاسكندرية":console.log("20% خصم ");break
+case "اسوان":console.log("50% خصم");break
+default :console.log("معليش المحافظة دي مش  معانا");
+
+
+
+
+}
